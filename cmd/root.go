e3aba3004c7a3cb8fab7864b2ebe6fded5aac597
Kyle Huggins <kyle@huggins.io>
2023-10-17 18:48:22 -0500

    feat: MVP

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,16 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"regexp"
+	"text/template"
 
 	"github.com/spf13/cobra"
 	"go.huggins.io/prj/git-vwi/lib/git"
+	"go.huggins.io/prj/git-vwi/lib/shell"
+	"go.huggins.io/prj/git-vwi/lib/util"
 )
 
 var rootCmd = &cobra.Command{
@@ -19,13 +24,50 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	PreRun: func(cmd *cobra.Command, _ []string) {
 		if cmd.Use != "version" {
-			// NOTE: Global preflight. Runs before any commands are executed,
-			// excluding built-in help messages.
+			// NOTE: Global preflight. Runs before any commands are executed, excluding built-in help messages.
 			git.Version()
+			git.RepositoryCheck()
 		}
 	},
 	Run: func(_ *cobra.Command, _ []string) {
-		fmt.Println("git-vwi")
+		urlConfig := git.GetConfig("git-view-work-item.url")
+		regexConfig := git.GetConfig("git-view-work-item.regex")
+
+		if len(urlConfig) == 0 {
+			fmt.Println("fatal: URL configuration property not found")
+			fmt.Println("use with -h or --help flag to view help")
+			os.Exit(4)
+		} else if len(regexConfig) == 0 {
+			fmt.Println("fatal: identifier regular expression configuration property not found")
+			fmt.Println("use with -h or --help flag to view help")
+			os.Exit(4)
+		}
+
+		currentBranch := git.CurrentBranch()
+		regex, err := regexp.Compile(regexConfig)
+		util.CheckErrorFatal(err, "regular expression failed to compile")
+
+		type Properties struct {
+			Identifier string
+		}
+
+		if !regex.MatchString(currentBranch) {
+			fmt.Println("warn: no matches for regular expression")
+			fmt.Println("")
+			fmt.Println("Current git branch: " + currentBranch)
+			fmt.Println("Regular expression: " + regex.String())
+			os.Exit(5)
+		}
+
+		var props Properties
+		props.Identifier = regex.FindString(currentBranch)
+
+		var finalUrl bytes.Buffer
+		templ := template.Must(template.New("templ").Parse(urlConfig))
+		templ.Execute(&finalUrl, props)
+
+		// TODO: make compatible with non-macOS environments
+		shell.Execute("open", finalUrl.String())
 	},
 }
 
@@ -38,4 +80,5 @@ func Execute() {
 
 func init() {
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	// TODO: provide flag that will copy the link instead of opening it in the browser
 }
--- lib/git/git.go
+++ lib/git/git.go
@@ -8,7 +8,11 @@ import (
 	"go.huggins.io/prj/git-vwi/lib/util"
 )
 
-var git404 string = "could not execute git"
+var (
+	git404         string = "could not execute git"
+	gitConfigUrl   string = "git-view-work-item.url"
+	gitConfigRegex string = "git-view-work-item.regex"
+)
 
 // Returns the version of git currently in your path.
 func Version() string {
@@ -20,4 +24,31 @@ func Version() string {
 	versionString := versionRegex.FindString(versionOutput)
 
 	return versionString
+}
+
+// No action if you are inside a repository. If you are not in a repository,
+// display a message to the user and exit the program.
+func RepositoryCheck() {
+	_, err := shell.Execute("git", "rev-parse", "--is-inside-work-tree")
+	util.CheckErrorFatal(err, "not a git repository (or any of the parent directories)")
+}
+
+// Returns the name of the current branch.
+func CurrentBranch() string {
+	out, err := shell.Execute("git", "branch", "--show-current")
+	util.CheckErrorFatal(err, git404)
+	return strings.TrimSpace(string(out))
+}
+
+// Get the value of a global config key.
+func GetConfig(key string) string {
+	// NOTE: potential errors ignored, since PersistentPreRun will have validated
+	// that Git is executable and we are in a repository.
+	out, err := shell.Execute("git", "config", "--get", key)
+
+	if err != nil {
+		return ""
+	} else {
+		return strings.TrimSpace(string(out))
+	}
 }
